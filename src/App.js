be5cb9d533ecaa52768d7facf0c5d13c7bb10d56
Rufused <perpetuum.coding@gmail.com>
2021-07-07 21:28:25 +0300

    les4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import Menu from './components/menu/Menu.js'
-import Users from "./components/users/Users";
+import {useDispatch, useSelector} from "react-redux";
+
+
+const {name, age, car, car: {model}} = {name: 'vasya', age: 23, car: {model: 'audi', color: 'red'}};
+console.log(name);
+console.log(car);
+console.log(model);
+
+const NestedChild = () => {
+    // const counter = useSelector((state) => {
+    // console.log('from useSelector', state)
+    // return state.counter.value
+    // })
+
+    // const counter = useSelector(state => console.log(state.counter))
+    // console.log(counter.counter.value);
+
+    const dispatch = useDispatch()
+    return(<div>
+        {/*{counter.counter.value}*/}
+        <button onClick={()=>{
+            dispatch({type:'INC'})
+        }}>inc</button>
+        <br/>
+        {2+1}
+    </div>);
+}
+
+
+
 
 
 export default function App() {
-    return (
-
-        <div className="App">
-            <Menu
-                pages={['user page', 'component page', 'posts page']}
-                classes={['target','xxx']}
-            />
-            <Users/>
-            <Menu
-                pages={['about','team']}
-                classes={['asd','target']}
-            />
-
-        </div>
-    );
+    return (<NestedChild/>)
 }
 
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,70 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
+const initialState = {
+    counter:{
+        value: 0
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC':{
+            return{...state,
+                counter:
+                    {value: state.counter.value+1}
+            }
+        }
+        case 'DEC':{
+            return {...state,
+            counter:
+                {value: state.counter.value-1}
+            }
+        }
+        case 'NUL':{
+            return {...state,
+            counter:
+                {value: state.counter.value=0}
+            }
+        }
+        // case 'CUST':{
+        //     return {...state,
+        //     counter:
+        //         {value: state.counter.value = custom}
+        //     }
+        // }
+        default:
+            return state;
+
+    }
+}
+
+
+const store = createStore(reducer)
+
+store.subscribe(()=>{
+   console.log(store.getState(),'------')
+})
+
+const INCREMENT_ACTION ={
+    type: 'INC'
+}
+
+store.dispatch(INCREMENT_ACTION)
+store.dispatch(INCREMENT_ACTION)
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={store}>
       <App/>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
